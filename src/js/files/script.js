2b35091c1ee36fb46a27935c5b37c8bffb880769
Dima <70588360+WebOriginals@users.noreply.github.com>
2022-06-15 20:34:38 +0300

    add fix header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,3 +1,13 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
+let header = document.querySelector('.header');
+if(header) {
+    window.onscroll = function () {
+        if (window.pageYOffset > 100) {
+            header.classList.add('stickytop');
+        } else {
+            header.classList.remove('stickytop');
+        }
+    };
+}
